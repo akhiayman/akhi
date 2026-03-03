9acfef66acd3306fdc24567b3e4e3a3bfa3ceb72
akhiayman <ayman.amisnaou@etu.uae.ac.ma>
2026-03-03 20:37:12 +0000

    Create service worker for offline functionality and caching strategies

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+// service-worker.js
+
+// Cache name
+const CACHE_NAME = 'v1';
+
+// Files to cache
+const CACHE_ASSETS = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Caching files');
+            return cache.addAll(CACHE_ASSETS);
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
